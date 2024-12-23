ca7e0387e4cd11d8c62f3a124901d9d30c10d262
Joseph L. Sumaylo <josephsumaylojjjj@gmail.com>
2024-12-23 12:02:43 +0800

    LA2

---
+++ LA's/LA2_OOP.py
@@ -0,0 +1,11 @@
+print("OOP_LA#2", "Name: Sumaylo, Joseph L. ")
+
+class MLBB:
+    def __init__(self, name, role ):
+        self.name = name
+        self.role = role
+       
+hero1 = MLBB("Hayabusa", "Assassin")
+
+print(hero1.name)
+print(hero1.role)
