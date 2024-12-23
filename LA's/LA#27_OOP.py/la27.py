7639abad528f9d361b829a750d357663034486a3
Joseph L. Sumaylo <josephsumaylojjjj@gmail.com>
2024-12-23 15:14:33 +0800

    la27.py

---
+++ LA's/LA#27_OOP.py/la27.py
@@ -0,0 +1,54 @@
+print("SUMAYLO-BSIT2B-LA27")
+
+from abc import ABC, abstractmethod
+
+class NinjaTurtle(ABC):
+    @property
+    @abstractmethod
+    def alias(self):
+        pass
+
+class Leonardo(NinjaTurtle):
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+class Michelangelo(NinjaTurtle):
+
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+class Donatello(NinjaTurtle):
+
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+class Rapheal(NinjaTurtle):
+
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+
+if __name__ == "__main__":
+
+    tur1 = Leonardo("Leonardo", "Leone")
+    print(tur1.name)
---
+++ LA's/LA#27_OOP.py/oop.py
@@ -0,0 +1,7 @@
+print("SUMAYLO-BSIT2B-LA27")
+
+import la27 as x
+
+hey = x.Donatello("Donatello", "Donna")
+
+print(hey.name)
